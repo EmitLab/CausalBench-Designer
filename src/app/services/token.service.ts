8cb355248481892831f0253907a4ceaa203f1da8
Pratanu Mandal <pratanu.mandal@gmail.com>
2025-07-09 19:54:49 -0700

    Added more debug outputs

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -8,12 +8,38 @@ export class TokenService {
 
   constructor() {
     this.loadToken();
+
+    if (!this.token) {
+      // window.open('https://causalbench.org', '_self');
+    }
   }
 
   private loadToken() {
     this.token = sessionStorage.getItem('token') || '';
+    
     if (!this.token) {
-      window.open('https://causalbench.org', '_self');
+      // When the app loads, ask for token from opener
+      if (window.opener) {
+        window.opener.postMessage('ready-for-token', 'https://causalbench.org');
+      }
+
+      console.log('Opener window:');
+      console.log(window.opener);
+
+      // Listen for the token from parent
+      window.addEventListener('message', event => {
+        console.log('Received message from parent window:');
+        console.log(event);
+        console.log(event.origin);
+
+        if (event.origin !== 'https://causalbench.org') return;
+
+        const token = event.data?.token;
+        if (token) {
+          sessionStorage.setItem('token', token);
+          // Optionally trigger login or route transition
+        }
+      });
     }
   }
 
